586d48b74eb281030738cc4c1b0279f43f2e60af
APONCE-101 <79687205+APONCE-101@users.noreply.github.com>
2021-02-26 20:27:09 -0800

    Add files via upload

---
+++ SwitchPractice.java
@@ -0,0 +1,35 @@
+public class SwitchPractice {
+
+    public static void main(String[] args) {
+
+        String fruit = "Grape";
+
+        switch (fruit) {
+
+            case "Apple":
+                System.out.println("Red");
+
+                break;
+
+            case "Orange":
+                System.out.println("Orange");
+
+                break;
+
+            case "Banana":
+                System.out.println("Yellow");
+
+                break;
+
+            case "Grape":
+                System.out.println("Green");
+
+                break;
+
+            case "Plum":
+                System.out.println("Purple");
+
+
+        }
+    }
+}
---
+++ TernaryPractice.java
@@ -0,0 +1,21 @@
+public class TernaryPractice {
+
+    public static void main(String[] args) {
+        int inputCal = 1000;
+        int goal = 500;
+        int calsResult = inputCal >= goal ? 1 : 0;
+
+        System.out.println(calsResult);
+
+        String student = "Dumbledore";
+        // if student is a witch or wizard, they are admitted
+        String admission = student.equals("witch") || student.equals("wizard") ? "Admission approved" : "Admission denied";
+
+        System.out.println(admission);
+
+
+
+
+    }
+}
+
